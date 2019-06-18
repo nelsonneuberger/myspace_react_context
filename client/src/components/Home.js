8ce25db501a545d7d4bbb245d961bf636af56993
Nelson Neuberger <nelson.neuberger@gmail.com>
2019-06-18 08:37:34 -0600

    init commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Header, } from "semantic-ui-react";
 
 const Home = () => (
-  <Header as="h3" textAlign="center">Cat Tinder</Header>
+  <Header as="h3" textAlign="center">LieSpace</Header>
 );
 
 export default Home;
